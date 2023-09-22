1aa92adb1d0889107409d81ad99c5c9a25c250df
Shosuke Noma <47355058+shosukenoma@users.noreply.github.com>
2023-09-22 00:19:27 -0500

    Implement card using struct

--- deck.go
+++ deck.go
@@ -2,19 +2,26 @@ package main
 
 import "fmt"
 
-type deck []string
+type card struct{
+	suit string
+	rank string
+	value int
+}
+type deck []card
 
 func newDeck(includeJoker bool) deck {
 	cards := deck{}
-	cardValues := []string{"A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"}
-	for i := 0; i < 4; i++ {
-		for _, value := range cardValues {
-			cards = append(cards, value)
+	cardSuits := []string{"Spades", "Hearts", "Clubs", "Diamonds"}
+	cardRanks := []string{"A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"}
+	for _, suit := range cardSuits {
+		for index, rank := range cardRanks {
+			card := card{suit, rank, index + 1}
+			cards = append(cards, card)
 		}
 	}
-	if includeJoker {
-		cards = append(cards, "Z", "Z")
-	}
+	// if includeJoker {
+	// 	cards = append(cards, "Z", "Z")
+	// }
 	return cards
 }
 
